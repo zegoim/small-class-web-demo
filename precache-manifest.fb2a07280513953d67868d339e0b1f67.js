1966319d1d40c88c1006c21662aea9c00e18ab8b
myxvisual <myxvisual@live.com>
2019-10-21 11:27:35 +0800

    feat: Update files

---
+++ precache-manifest.fb2a07280513953d67868d339e0b1f67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e85b53aa8dafc436587a9bfa41e36c",
+    "url": "/small-class-web-demo/index.html"
+  },
+  {
+    "revision": "9e957aedebba6e5be60d",
+    "url": "/small-class-web-demo/static/css/main.6d67e4fa.chunk.css"
+  },
+  {
+    "revision": "56780a97ad3cfe78fb41",
+    "url": "/small-class-web-demo/static/js/0.738aa66e.chunk.js"
+  },
+  {
+    "revision": "e41d46570edac97bf147",
+    "url": "/small-class-web-demo/static/js/1.07705dfd.chunk.js"
+  },
+  {
+    "revision": "ac5aeeba4ddeba116b34",
+    "url": "/small-class-web-demo/static/js/4.23ae9f62.chunk.js"
+  },
+  {
+    "revision": "805414e514b9eb5d56bf",
+    "url": "/small-class-web-demo/static/js/5.8f2db403.chunk.js"
+  },
+  {
+    "revision": "58f3273bd17b944a5c6a",
+    "url": "/small-class-web-demo/static/js/6.e76ea611.chunk.js"
+  },
+  {
+    "revision": "9e957aedebba6e5be60d",
+    "url": "/small-class-web-demo/static/js/main.93be7d26.chunk.js"
+  },
+  {
+    "revision": "044fecb1c8a0fb337f2d",
+    "url": "/small-class-web-demo/static/js/runtime~main.284b5129.js"
+  },
+  {
+    "revision": "5d33f942880587908ce02f1587dbdbe2",
+    "url": "/small-class-web-demo/static/media/02-bg.5d33f942.jpg"
+  }
+]);
